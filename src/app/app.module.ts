5c1af7e5939b596240277bcb61ecd83ef93029b1
Amon Keishima <amon.keishima@gmail.com>
2017-03-12 07:28:15 +0900

    【provider追加】記事サービスを使えるようにappmodule内で定義した

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ArticleService } from "./article.service";
 
 @NgModule({
   declarations: [
@@ -16,7 +17,7 @@ import { AppComponent } from './app.component';
     HttpModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [ArticleService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
